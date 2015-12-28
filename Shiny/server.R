58ae3a44307d2bb7d3026c82708ec5f3998b1389
mbramson <mathewbramson@gmail.com>
2015-12-27 22:55:52 -0500

    implement better loading in server.R

--- Shiny/server.R
+++ Shiny/server.R
@@ -1,11 +1,19 @@
 # Load packages
 library(shiny)
-library(tm)
+library(quanteda)
 
-# Load the source data
-con <- file("en_US.twitter.txt", "r")
-tweets <- readLines(con, 2360148)
-close(con)
+# Load Corpora
+tweet.con <- file("en_US.twitter.txt")
+tweet.corpus <- corpus(readLines(tweet.con))
+close(tweet.con)
+
+blog.con <- file("en_US.blogs.txt")
+blog.corpus <- corpus(readLines(blog.con))
+close(blog.con)
+
+news.con <- file("en_US.news.txt", open="rb")
+news.corpus <- corpus(readLines(news.con, encoding="UTF-8"))
+close(news.con)
 
 # Output
 shinyServer(function(input,output) {
